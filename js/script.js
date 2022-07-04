3eeec68885807e69867fe272b2582fe935c760f4
Tavaresds1 <103668960+Tavaresds1@users.noreply.github.com>
2022-07-03 22:45:09 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,66 @@
+(function ($) {
+  'use strict';
+  $(window).on('load', function () {
+    $('#preloader').fadeOut('slow', function () {
+      $(this).remove();
+    });
+  });
+
+
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    //console.log(scroll);
+    if (scroll > 200) {
+      //console.log('a');
+      $('.navigation').addClass('sticky-header');
+    } else {
+      //console.log('a');
+      $('.navigation').removeClass('sticky-header');
+    }
+  });
+
+    setTimeout(function(){
+      var containerEl = document.querySelector('.filtr-container');
+      var filterizd;
+      if (containerEl) {
+        filterizd = $('.filtr-container').filterizr({});
+      }
+    }, 500);
+
+
+
+
+
+  var scroll = new SmoothScroll('a[href*="#"]');
+
+  function counter() {
+    if ($('.counter').length !== 0) {
+      var oTop = $('.counter').offset().top - window.innerHeight;
+    }
+    if ($(window).scrollTop() > oTop) {
+      $('.counter').each(function () {
+        var $this = $(this),
+          countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
+          countNum: countTo
+        }, {
+          duration: 1000,
+          easing: 'swing',
+          step: function () {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function () {
+            $this.text(this.countNum);
+          }
+        });
+      });
+    }
+  }
+
+  $(window).scroll(function () {
+    counter();
+  });
+
+})(jQuery);
